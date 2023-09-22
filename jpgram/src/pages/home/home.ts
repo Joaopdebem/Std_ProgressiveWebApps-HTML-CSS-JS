2411856714b2f1d8dce7bb8960a39a3c39deece7
João Pedro de Bem <jpdebem@gmail.com>
2023-09-22 09:04:34 -0300

    att

---
+++ jpgram/src/pages/home/home.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { NavController, ModalController } from 'ionic-angular';
+
+import {PhotosPage} from '../photos/photos';
+import {TakePicturePage} from '../take-picture/take-picture';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  public photosTab: any;
+  public profileTab: any;
+
+  constructor(
+    private navCtrl: NavController,
+    private modalCtrl: ModalController
+    )
+    {
+      this.photosTab =PhotosPage;
+      this.profileTab = PhotosPage;
+    }
+
+  showSendPhoto(){
+    let modal = this.modalCtrl.create(TakePicturePage);
+    modal.present();
+  }
+
+}
